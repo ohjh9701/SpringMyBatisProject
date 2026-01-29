baa479604620452ed7ad983dc362a78e9591fa28
ohjh9701 <ohjh4875@gmail.com>
2026-01-29 17:45:30 +0900

    [추가] BoardServiceImpl 추가

---
+++ SpringMyBatisProject/src/main/java/com/zeus/service/BoardServiceImpl.java
@@ -0,0 +1,57 @@
+package com.zeus.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.zeus.domain.Board;
+import com.zeus.mapper.BoardMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class BoardServiceImpl implements BoardService {
+	
+	@Autowired
+	private BoardMapper boardMapper;
+
+	@Override
+	@Transactional
+	public void create(Board b) throws Exception {
+		boardMapper.create(b);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Board read(Integer no) throws Exception {
+		return boardMapper.read(no);
+	}
+
+	@Override
+	@Transactional
+	public void update(Board b) throws Exception {
+		boardMapper.update(b);
+	}
+
+	@Override
+	@Transactional
+	public void delete(Integer no) throws Exception {
+		boardMapper.delete(no);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Board> list() throws Exception {
+		return boardMapper.list();
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Board> boardSearch(Board board) throws Exception {
+		return boardMapper.boardSearch(board);
+	}
+
+}
